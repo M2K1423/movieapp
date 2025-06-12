60e67970664295f40f7afe54d44dcc5fbcc54984
tienphat22130205 <22130205@st.hcmuaf.edu.vn>
2025-06-12 22:08:26 +0700

    intro

---
+++ app/src/main/java/com/example/movieapp/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.movieapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.movieapp.activity.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        // Áp dụng padding cho hệ thống
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Gán sự kiện cho nút Start
+        Button btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(view -> {
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish(); // Kết thúc splash để không quay lại được
+        });
+    }
+}
