c39ab00d5e26c36f113e554bb8024b45d9b4872a
tienphat22130205 <22130205@st.hcmuaf.edu.vn>
2025-06-16 22:46:30 +0700

    intro

--- app/src/main/java/com/example/movieapp/adapters/CategoryAdapter.java
+++ app/src/main/java/com/example/movieapp/adapters/CategoryAdapter.java
@@ -1,4 +1,4 @@
-//package com.example.movieapp.adaptors;
+package com.example.movieapp.adapters;//package com.example.movieapp.adaptors;
 //
 //import android.content.Context;
 //import android.view.LayoutInflater;
